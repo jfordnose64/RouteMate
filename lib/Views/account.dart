177913f43fab3fe4c9979c04542b9cde2f587b71
jackson ford <jfordnose64@gmail.com>
2020-07-22 12:21:16 -0400

    more flutter learning

--- lib/Views/account.dart
+++ lib/Views/account.dart
@@ -1,22 +1,20 @@
-import 'package:RouteMate/app.dart';
-import 'package:RouteMate/data_models/user_location.dart';
-import 'package:RouteMate/services/location_service.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<UserLocation>(
-      builder: (context) => LocationService().locationStream,
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+    return (Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Message',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.blue,
           ),
-          home: Scaffold(
-            body: App(),
-          )),
-    );
+        ),
+      ),
+    ));
   }
 }
--- lib/Views/message.dart
+++ lib/Views/message.dart
@@ -3,18 +3,44 @@ import 'package:flutter/material.dart';
 class Message extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Message',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 50,
-            color: Colors.blue,
-          ),
+    return Scaffold(
+      appBar: AppBar(title: Text('RouteMate')),
+      body: Center(child: Text('Home!')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
-    ));
+    );
   }
 }
